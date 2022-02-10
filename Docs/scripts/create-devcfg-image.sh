b03f82a577b47eb7b7741a66e9b23fd16036a372
Theodore A. Roth <troth@openavr.org>
2022-02-09 20:48:42 -0700

    Add dm-verity with root hash signing to devcfg images
    When creating the devcfg squashfs images:
    
    * Add dm-verity hash trees to the images.
    * Sign the Root Hash with a signing key.
    * Add dm-verity info, signing crt and signature of the info to a tarball
      appended to the squashfs images.
    
    This provides the means for validation of the devcfg images before they
    are mounted into the system.
    
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- Docs/scripts/create-devcfg-image.sh
+++ Docs/scripts/create-devcfg-image.sh
@@ -2,6 +2,29 @@
 
 INSTALL="${1:-noinstall}"
 
+SIGNING_KEY="${SIGNING_KEY:-$(realpath devcfg-signing.key)}"
+SIGNING_CRT="${SIGNING_CRT:-$(realpath devcfg-signing.crt)}"
+
+#
+# Convert the output of `veritysetup format` into "Key=Value" data which can
+# be sourced into a shell script for easier access.
+#
+function process_verity()
+{
+    read header
+    printf "#\n# $header\n#\n"
+
+    local IFS=":"
+    while read KEY VAL
+    do
+        printf "%s=%s\n" \
+            "$(echo "$KEY" | tr '[:lower:]' '[:upper:]' | tr ' ' '_')" \
+            "$(echo "$VAL" | tr -d ' \t')"
+    done
+    echo "DATA_SIZE=${SIZE}"
+}
+
+
 function create_devcfg_image()
 {
     DEV_SER_NUM="${1:-0001}"
@@ -15,17 +38,20 @@ function create_devcfg_image()
     echo "############################################"
 
     BASE="devcfg-${DEV_SER_NUM}"
-    DEVCFG_DIR="${BASE}"
+    rm -rf "${BASE}"
+    mkdir -p "${BASE}"
+    pushd "${BASE}"
+
+    ln -s ${SIGNING_CRT} devcfg-signing.crt
+
+    DEVCFG_DIR="devcfg"
     IMAGE_NAME="${BASE}.squashfs"
 
-    MACH_ID_SRC="Machine-ids/machine-id-${BASE}"
+    MACH_ID_SRC="../Machine-ids/machine-id-${BASE}"
     MACH_ID_DST="${DEVCFG_DIR}/etc/machine-id"
 
     set -e
     set -x
-
-    rm -f "${IMAGE_NAME}"
-    rm -rf "${BASE}"
 
     mkdir -p "${DEVCFG_DIR}/etc"
 
@@ -52,12 +78,35 @@ function create_devcfg_image()
 	EOF
 
     #
-    # Need to use `-all-root` so that no files/dirs are created as user running the
-    # script (which causes ownership issues when the overlayfs is created on the
-    # device.
+    # Need to use `-all-root` so that no files/dirs are created as user running
+    # the script (which causes ownership issues when the overlayfs is created
+    # on the device.
     #
     mksquashfs "${DEVCFG_DIR}" "${IMAGE_NAME}" -all-root
-    set +x
+
+    #
+    # Create the dm-verity hashing data.
+    #
+    SIZE=$(stat -c '%s' ${IMAGE_NAME})
+    veritysetup format --hash-offset ${SIZE} ${IMAGE_NAME} ${IMAGE_NAME} \
+        | process_verity > ${IMAGE_NAME}.info
+
+    (
+        source ${IMAGE_NAME}.info
+        veritysetup verify --hash-offset ${DATA_SIZE} ${IMAGE_NAME} \
+            ${IMAGE_NAME} ${ROOT_HASH}
+    ) || return 1
+
+    openssl dgst -sha256 -sign ${SIGNING_KEY} ${IMAGE_NAME}.info \
+        > ${IMAGE_NAME}.info.sig
+
+    # Append a 4K block to the squashfs image which contains a tarball of the
+    # crt, dm-verity info the signature of the dm-verify info.
+
+    tar chzf ${IMAGE_NAME}.tgz --owner=root:0 --group=root:0 \
+        ${IMAGE_NAME}.info ${IMAGE_NAME}.info.sig devcfg-signing.crt
+
+    dd conv=sync bs=4K if=${IMAGE_NAME}.tgz >> ${IMAGE_NAME}
 
     if [ "${INSTALL}" == "install" ]
     then
@@ -67,10 +116,17 @@ function create_devcfg_image()
     fi
 }
 
+if [ ! -f ${SIGNING_KEY} ]
+then
+    echo "ERROR: No signing key found: ${SIGNING_KEY}"
+    exit 1
+fi
+
 mkdir -p Machine-ids
 
 for i in $(seq 1 6)
 do
     sernum=$(printf "%04d" "${i}")
     create_devcfg_image "${sernum}"
+    popd
 done
